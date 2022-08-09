8e88e134129a54f833f1ca93bc1bc024d58a3b63
Bob Carr <robert.carr@orau.org>
2022-08-09 16:42:07 -0400

    add dynamic tags(incomplete)

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -6,10 +6,16 @@ export default {
   },
   template: `
   <section v-show="assignments.length">
-  <h2 class="font-bold mb-2">{{title}}</h2>
+  <div class="flex justify-between items-center mb-2">
+  <h2 class="font-bold mb-2">{{title}}  </h2>
+  <span class="rounded-full bg-red-600 px-2 ml-2">{{assignments.length}}</span>
+  </div>
+  <div class="flex gap-2">
+  <button @click="currentTAg = tag" v-for="tag in tags" class="border rounded px-1 py-px text-xs">{{tag}}</button>
+  </div>
   <ul class="border border-gray-600 rounded divide-y divide-gray-600">
       <assignment 
-      v-for="assignment in assignments"
+      v-for="assignment in filteredAssignments"
       :key="assignment.id"
       :assignment="assignment"
       ></assignment>
@@ -19,5 +25,18 @@ export default {
   props: {
     assignments: Array,
     title: String,
+  },
+  data() {
+    return {
+      currentTag: "",
+    };
+  },
+  computed: {
+    tags() {
+      return new Set(this.assignments.map((a) => a.tag));
+    },
+    filteredAssignments() {
+      return this.assignments.filter((a) => a.tag === this.currentTag);
+    },
   },
 };
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -14,11 +14,11 @@ export default {
   data() {
     return {
       assignments: [
-        { name: "Finish project", complete: false, id: 1 },
-        { name: "Read chapter four", complete: false, id: 2 },
-        { name: "Turn in homework", complete: false, id: 3 },
-        { name: "Get groceries", complete: false, id: 4 },
-        { name: "Go to bank", complete: false, id: 5 },
+        { name: "Finish project", complete: false, id: 1, tag: "math" },
+        { name: "Read chapter four", complete: false, id: 2, tag: "science" },
+        { name: "Turn in homework", complete: false, id: 3, tag: "reading" },
+        { name: "Get groceries", complete: false, id: 4, tag: "science" },
+        { name: "Go to bank", complete: false, id: 5, tag: "math" },
       ],
     };
   },
